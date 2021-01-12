dbf81262f982a39841bb05ce7129170ae978e12b
yahya youness <yahyayounes@hotmail.fr>
2021-01-12 17:35:30 +0100

    Add route to generate token for livret scolaire application
    Co-authored-by: ceol <antoine.ceol@pix.fr>

---
+++ api/lib/infrastructure/open-api-doc/authentication/response-authentication-doc.js
@@ -0,0 +1,7 @@
+const Joi = require('@hapi/joi');
+
+module.exports = Joi.object({
+  token_type: 'bearer',
+  'access_token': Joi.string().required().description('The bearer token.'),
+  'client_id': Joi.string().required().description('The client id.'),
+}).label('BearerToken');
