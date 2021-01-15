320d56a00632ed6ecb3b5a896bbbdb9537f10500
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-01-15 15:03:25 +0100

    chore: glimmerize certification-not-certifiable-test

--- mon-pix/tests/integration/components/certification-not-certifiable-test.js
+++ mon-pix/tests/integration/components/certification-not-certifiable-test.js
@@ -9,7 +9,7 @@ describe('Integration | Component | certification-not-certifiable', function() {
 
   it('renders', async function() {
 
-    await render(hbs`{{certification-not-certifiable}}`);
+    await render(hbs`<CertificationNotCertifiable />`);
 
     expect(find('.certification-not-certifiable__title').textContent.trim()).to.equal('Votre profil n\'est pas encore certifiable.');
     expect(find('.certification-not-certifiable__text').textContent.trim()).to.equal('Pour faire certifier votre profil, vous devez avoir obtenu un niveau supérieur à 0 dans 5 compétences minimum.');
